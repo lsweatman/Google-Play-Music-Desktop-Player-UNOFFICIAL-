02e6b9d4313f27259d51b744032cb244b400303e
Luke <lukesweatman@gmail.com>
2017-03-27 17:16:30 -0700

    Added Volume Slider

--- src/server/components/ConnectButtons.js
+++ src/server/components/ConnectButtons.js
@@ -4,8 +4,8 @@ import Button from 'react-bootstrap/lib/Button';
 export default class ConnectButtons extends React.Component {
     render() {
         return (
-            <div className="connect-buttons">
-                <Button className="btn btn-primary"
+            <div className="connect-buttons base-div">
+                <Button className="btn btn-primary button-no-drag"
                         onClick={this.props.connectionClicked}>
                     {this.props.connectionStatus}
                 </Button>
